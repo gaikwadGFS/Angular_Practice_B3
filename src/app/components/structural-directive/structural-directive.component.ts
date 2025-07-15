c059bcb85da123e816e6588f8b8fde0a1c7abc19
gaikwadGFS <kshitija.gaikwad@growingflowers-solutions.com>
2025-07-15 11:28:55 +0530

    *ngFor session

--- src/app/components/structural-directive/structural-directive.component.ts
+++ src/app/components/structural-directive/structural-directive.component.ts
@@ -16,6 +16,41 @@ export class StructuralDirectiveComponent {
   num1:string ="";
   num2:string ="";
   active:boolean = true;
+  city:any[] = ['Pune','Mumbai','Thane','Solapur'];
+  rollNo:any[]=[11, 12,13,14,15,16,17,18,19,20];
+
+  studentDetails:any[]=[
+    {
+      name:'Yash',
+      age:20,
+      city:'Pune',
+      isActive:true
+    },
+     {
+      name:'Shubham',
+      age:25,
+      city:'Mumbai',
+      isActive:false
+    },
+     {
+      name:'Ram',
+      age:30,
+      city:'Thane',
+      isActive:true
+    },
+     {
+      name:'Shyam',
+      age: 15,
+      city:'Solpaur',
+      isActive:false
+    },
+     {
+      name:'Shreya',
+      age:21,
+      city:'Pandharpur',
+      isActive:true
+    },
+  ]
 
   hideDiv1(){
     this.isDivVisible1 = false;
